3a2d9181ae58e52d4c0e5e8f591c573aafa5aef4
ticooo95 <ahmed1995bazzara@gmail.com>
2018-02-27 19:00:51 +0100

    API KEY added and data is fetched properly from the server

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
+
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
-class App extends React.Component{
-  render(){
-    return(
+
+const API_KEY = "4623bb99a9cd68459b3f8e5f3267cae4";
+class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+    
+    const data = await api_call.json();
+     console.log(data);
+  }
+  render() {
+    return (
       <div>
         <p>Hello!</p>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
 
       </div>
